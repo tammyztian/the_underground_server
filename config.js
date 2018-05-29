67a2d927cbc48b463c8bd886485e242002ed1f62
Tammy Tian <tiantammy@outlook.com>
2018-05-29 22:29:50 +0200

    mounted mLab database

--- config.js
+++ config.js
@@ -3,11 +3,8 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
-  TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://user1:user1@ds239930.mlab.com:39930/the_underground',
+  TEST_DATABASE_URL:process.env.TEST_DATABASE_URL ||'mongodb://localhost/thinkful-backend-test'
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
