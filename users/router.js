1b04e804a75d9f8e2270286caecc5aeb089bb804
Tammy Tian <tiantammy@outlook.com>
2018-05-29 22:30:20 +0200

    created POST to create user

--- users/router.js
+++ users/router.js
@@ -1,4 +1,117 @@
 'use strict';
 
 const express = require('express');
+const router = express.Router();
+
 const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {User} = require('./models');
+
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if (missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'Validation Error',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
+
+  const stringFields = ['username', 'password', 'firstName', 'lastName'];
+  const nonStringField = stringFields.find(field => field in req.body && typeof req.body[field] !== 'string');
+
+  if (nonStringField) {
+    return res.status(422).json({
+      code:422,
+      reason: 'ValidationError',
+      message:'Incorrect field type: expected sting',
+      location: nonStringField
+    });
+  }
+  
+  const expilicitlyTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = expilicitlyTrimmedFields.find(
+    field => res.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField){
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    username: {
+      min: 3
+    },
+    password: {
+      min: 10,
+      max: 25
+    }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 'min' in sizedFields[field] && req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => 'max' in sizedFields[field]  && req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooLargeField || tooSmallField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooLargeField ? `Must be at most ${sizedFields[tooLargeField].max} characters long` :
+        `Must be at least ${sizedFields[tooSmallField].min} characters long`,
+      location: tooLargeField || tooSmallField
+    });
+  }
+
+  let {username, password, firstName = '', lastName = ''} = req.body;
+
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+
+  return User.find({username})
+    .count()
+    .then(count => {
+      if(count > 0) {
+        return Promise.reject({
+          code:422,
+          reason: 'ValidaitonError',
+          message: 'Username already taken',
+          location: 'username'
+        });
+      }
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password:hash,
+        firstName,
+        lastName
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
+    })
+    .catch(err => {
+      if (err.reason === 'ValidationError'){
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({
+        code: 500,
+        message:'Internal server error'
+      });
+    });
+});
+
+module.exports = {router};
