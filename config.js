2719af823ec6b3a2339c47f30a80d848d1eab60e
Tammy Tian <tiantammy@outlook.com>
2018-06-01 22:26:19 +0200

    added jwt settings

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL: process.env.DATABASE_URL || 'mongodb://user1:user1@ds239930.mlab.com:39930/the_underground',
-  TEST_DATABASE_URL:process.env.TEST_DATABASE_URL ||'mongodb://localhost/thinkful-backend-test'
+  TEST_DATABASE_URL:process.env.TEST_DATABASE_URL ||'mongodb://localhost/thinkful-backend-test',
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '24h'
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
