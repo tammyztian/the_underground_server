99e97e04ea56d32bba7da9c0c6c20100d1a8b1d1
Tammy Tian <tiantammy@outlook.com>
2018-06-01 22:42:56 +0200

    added local strategy

--- auth/strategies.js
+++ auth/strategies.js
@@ -5,7 +5,36 @@ const {Strategy: JwtStrategy, ExtractJwt} = require('passport-jwt');
 const {User} = require('../users/userDataModel');
 const {JWT_SECRET} = require('../config');
 
-const localstrategy = ;
+const localstrategy = new localStrategy ((username, password, callback) =>{
+  let user;
+  //define user to make accecssable outside call
+  User.findOne({username: username})
+    .then(_user=>{
+      user =_user;
+      if (!user){
+        return Promise.reject({
+          reason: 'LoginError',
+          message:'Incorrect username or password'
+        });
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message:'Incorrect username or password'
+        });
+      }
+      return callback(null, user);
+    })
+    .catch(err => {
+      if (err.reason === 'LoginError'){
+        return callback(null, false, err);
+      }
+      return callback(err, false);
+    });
+});
 
 const jwtstrategy =;
 
