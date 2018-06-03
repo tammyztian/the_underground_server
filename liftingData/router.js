c2c24bfc217ba1b16472caa720a6817b5250b988
Tammy Tian <tiantammy@outlook.com>
2018-06-03 04:13:03 +0200

    protected endpoint for unique user with jwt auth

--- liftingData/router.js
+++ liftingData/router.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
+
+
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
@@ -12,7 +16,9 @@ const LiftingData = require('./liftingDataSchema');
 //retrieve lifting data
 
 router.get('/', jsonParser, (req, res, next) => {
-  LiftingData.find()
+  const userId = req.user._id;
+  let filter = {userId};
+  LiftingData.find(filter)
     .then(result => {
       if (result) {
         res.json(result);
@@ -28,8 +34,9 @@ router.get('/', jsonParser, (req, res, next) => {
 //post lifting data
 
 router.post('/', jsonParser, (req, res, next) => {
+  const userId = req.user._id;
   const {deadlift, bench, squat} = req.body;
-  LiftingData.create({deadlift, bench, squat})
+  LiftingData.create({deadlift, bench, squat, userId})
     .then(result => {
       res
         .location(`${req.originalUrl}/${result.id}`)
