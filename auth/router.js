e1cbfae44662ec2f5f481ce331f9ff54001c537f
Tammy Tian <tiantammy@outlook.com>
2018-06-03 03:39:13 +0200

    added bodyParser

--- auth/router.js
+++ auth/router.js
@@ -1,12 +1,29 @@
 'use strict';
-
 const express = require('express');
-const router = express.Router();
-
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET, JWT_EXPIRY } = require('../config');
+const {JWT_SECRET, JWT_EXPIRY} =  require('../config');
+const router = express.Router();
+
+
+const options = {session: false, failWithError: true};
+const localAuth = passport.authenticate('local', options);
+
+const createAuthToken = function(user) {
+  return jwt.sign({user}, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY,
+  });
+};
+
+
+router.use(bodyParser.json());
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({authToken});
+});
+
 
 module.exports = router;
