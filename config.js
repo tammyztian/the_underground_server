095222499a8a2a8d84e57593e141a96849d11857
Tammy Tian <tiantammy@outlook.com>
2018-06-03 03:41:01 +0200

    required dotenv

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict';
+require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
@@ -6,7 +7,7 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'mongodb://user1:user1@ds239930.mlab.com:39930/the_underground',
   TEST_DATABASE_URL:process.env.TEST_DATABASE_URL ||'mongodb://localhost/thinkful-backend-test',
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '24h'
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
