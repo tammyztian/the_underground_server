18b65bb15d68240f16b67bfe6eb16a4fe1f929d0
Tammy Tian <tiantammy@outlook.com>
2018-06-08 22:32:24 +0200

    debugging travis

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL: process.env.DATABASE_URL || 'mongodb://user1:user1@ds239930.mlab.com:39930/the_underground',
   TEST_DATABASE_URL:process.env.TEST_DATABASE_URL ||'mongodb://user01:user01@ds151970.mlab.com:51970/the_underground_test',
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || 'default',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
