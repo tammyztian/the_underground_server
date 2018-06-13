56fe1a2898be5d531d646362918caa1614460c44
Tammy Tian <tiantammy@outlook.com>
2018-06-13 22:23:57 +0200

    debugging deploy

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://theunderground.netlify.com/',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://theunderground.netlify.com',
   DATABASE_URL: process.env.DATABASE_URL || 'mongodb://user1:user1@ds239930.mlab.com:39930/the_underground',
   TEST_DATABASE_URL:process.env.TEST_DATABASE_URL ||'mongodb://user01:user01@ds151970.mlab.com:51970/the_underground_test',
   JWT_SECRET: process.env.JWT_SECRET || 'default',
